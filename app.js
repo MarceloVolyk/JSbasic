b0bb7362363caf9a64e69148fd6f7ea8c1afc629
Marcelo Volyk <marcelovolyk@gmail.com>
2024-09-10 18:24:49 -0300

    second commit with gitignore

--- app.js
+++ app.js
@@ -10,3 +10,9 @@
 //package.json - manifest file (stores important info about project/package)
 //manual approach: npm init
 //default approach (automatic): npm init -y
+
+//Gitignore will not upload "node_modules" to github
+//All installed node modules will be logged in the package.json file, under "dependencies"
+//When someone pulls this project from github, the person will just need to type "npm install" in the console,
+//and all modules listed under dependecies in package.json will be installed
+const _ = require('lodash')
